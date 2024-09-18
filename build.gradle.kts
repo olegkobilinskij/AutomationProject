34eac71fcd716a2dc67bb68e637f226973bb8438
olegKobilinskiy <oleg.kobylynskiy@better-test.net>
2024-09-18 18:43:51 +0300

    fgfgfgf

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.c4b"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.codeborne:selenide:7.5.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.3.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
