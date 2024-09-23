8748c625ccaefd92195177928b17c5468ad941d7
olegKobilinskiy <oleg.kobylynskiy@better-test.net>
2024-09-23 09:09:21 +0300

    search tests

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,17 +1,67 @@
+import static com.codeborne.selenide.Condition.text;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 
 public class SearchTests {
-    @Test
-    void successful3SearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    @BeforeEach
+    void beforeEach() {
+        String username = "cfw2";
+        String password = "AoiMa-47is";
+        String url = "https://" + username + ":" + password + "@stage.carsfromwest.com";
+        open(url);
+    }
 
+    @AfterEach
+    void tearDown() {
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @Test
+    void successfulVinSearch(){
+        $("#navbar-search-1").click();
+        $("#navbar-search-1").setValue("5TEJU62N36Z226840");
+        $(".me-sm-2").click();
+        $(".order-lg-last").shouldHave(text("2006 Toyota Tacoma"));
+
+    }
+
+    @Test
+    void successfulLotNumberSearch(){
+        $("#navbar-search-1").click();
+        $("#navbar-search-1").setValue("38669822");
+        $(".me-sm-2").click();
+        $(".order-lg-last").shouldHave(text("2006 Toyota Tacoma"));
+    }
+
+    @Test
+    void successfulNameCarSearch(){
+        $("#navbar-search-1");
+        $("#navbar-search-1").setValue("Toyota Tacoma");
+        $(".me-sm-2").click();
+        $("button.link").shouldHave(text("Toyota Tacoma"));
+    }
+
+    @Test
+    void successSearchWithTub(){
+        $("#navbar-search-1");
+        $("#navbar-search-1").setValue("    5TEJU62N36Z226840   ");
+        $(".me-sm-2").click();
+        $(".order-lg-last").shouldHave(text("Toyota Tacoma"));
+    }
+
+    @Test
+    void successSearchWithSpace(){
+        $("#navbar-search-1");
+        $("#navbar-search-1").setValue(" 5TEJU62N36Z226840 ");
+        $(".me-sm-2").click();
+        $(".order-lg-last").shouldHave(text("Toyota Tacoma"));
     }
 }
