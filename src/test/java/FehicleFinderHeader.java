bd45cdb4aa8d5499807dcc643ba0a0b31294da27
olegKobilinskiy <oleg.kobylynskiy@better-test.net>
2024-09-24 09:57:58 +0300

    Fehicle finder tests

---
+++ src/test/java/FehicleFinderHeader.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FehicleFinderHeader {
+    @BeforeEach
+    void beforeEach() {
+        String username = "cfw2";
+        String password = "AoiMa-47is";
+        String url = "https://" + username + ":" + password + "@stage.carsfromwest.com";
+        open(url);
+    }
+
+    @AfterEach
+    void tearDown() {
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @Test
+        void VehicleFinderListShouldBEVisible(){
+        $("#navbar-toggler-btn").click();
+        $(".list-unstyled").shouldBe(Condition.visible);
+    }
+
+    @Test
+        void clickOnTheOurPicks(){
+        $("#navbar-toggler-btn").click();
+        $(".list-unstyled").shouldBe(Condition.visible);
+        $$(".list-unstyled li").first().hover();
+    }
+}
