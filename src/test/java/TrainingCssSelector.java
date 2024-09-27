3192320cd2891a1ac6ffd64406e5fae8aa710409
olegKobilinskiy <oleg.kobylynskiy@better-test.net>
2024-09-27 09:55:47 +0300

    new nest search button

--- src/test/java/TrainingCssSelector.java
+++ src/test/java/TrainingCssSelector.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TrainingCssSelector {
     @BeforeEach
@@ -29,5 +31,22 @@ public class TrainingCssSelector {
         $("#home-register-button").click();
         $("#home-register-email-error").shouldHave(Condition.text("User with this email already registered."));
     }
+
+    @Test
+    void  searchButtonCheck() {
+        $("#home-search-button").shouldHave(Condition.text("Start search")).click();
+        assertEquals(WebDriverRunner.url(), "https://stage.carsfromwest.com/en/search?auctions=copart%2Ciaai&type=automobile&onlyActive=true&yearMin=1996&yearMax=2025");
+        $("[itemprop='name']").shouldHave(Condition.text("Home"));
+    }
+
+//    @Test
+//    void searchWithBuyNowButton () {
+//        // Находим чекбокс и кликаем по нему, чтобы активировать
+//        SelenideElement checkbox = $("[type='checkbox']"); // локатор чекбокса
+//        if (!checkbox.isSelected()) {
+//            checkbox.click(); // кликаем, чтобы установить флажок
+//        }
+//        checkbox.shouldBe(Condition.selected); // проверяем, что чекбокс выбран
+//    }
 
 }
